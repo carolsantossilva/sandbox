0a328ed2bc2d8d8749325f8aa6da0243598585f6
Ana Carolina Silva <ana.carolinasantos@icloud.com>
2023-03-09 14:26:33 -0300

    functions

---
+++ kotlin/fundamentos/src/main/kotlin/funcoes.kt
@@ -0,0 +1,18 @@
+fun main() {
+    println("Hello World")
+    var nome = returnName()
+    println(nome)
+    dizOi("Carol", 27)
+
+    dizOi(idade = 25, nome = "Diogo")
+
+    dizOi(idade = 18)
+}
+
+fun returnName(): String {
+    return "Carol"
+}
+
+fun dizOi(nome: String = "pessoa", idade: Int) {
+    println("Oi ${nome} - idade ${idade}")
+}
