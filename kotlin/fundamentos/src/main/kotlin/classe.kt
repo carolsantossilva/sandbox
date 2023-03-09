6362773711dbfd4cad5762be5089ca1eb0db6d9c
Ana Carolina Silva <ana.carolinasantos@icloud.com>
2023-03-09 12:26:32 -0300

    Class foundation

---
+++ kotlin/fundamentos/src/main/kotlin/classe.kt
@@ -0,0 +1,10 @@
+class Pessoa(var nome: String, var idade: Int) {
+    override fun toString(): String {
+        return "Classe: Pessoa Nome: ${nome} Idade: ${idade}"
+    }
+}
+
+fun main() {
+    val carol = Pessoa("Carol", 27)
+    println(carol)
+}
