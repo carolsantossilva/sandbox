bb629755d3b6410d8606f61b8b14540403f3f71e
Ana Carolina Silva <ana.carolinasantos@icloud.com>
2023-03-13 10:56:45 -0300

    variables

---
+++ kotlin/fundamentos/src/main/kotlin/variaveis.kt
@@ -0,0 +1,16 @@
+fun main() {
+    var nome = "Carol"
+    val nomeVal = "Carol"
+
+    nome = "Ana Carolina"
+    // nomeVal = "Ana Carolina" // nao pode, pois val = constante
+
+    val teste: String
+    teste = "teste"
+}
+
+class variaveis {
+    val testeVal: String = "Carol" // precisa inicializar
+    var testeVar: String = "Carol"
+    lateinit var teste: String
+}
